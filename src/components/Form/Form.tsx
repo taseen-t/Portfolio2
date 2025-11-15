150d034cbdf6b1a81c044dc3eb48f769d6267bad
Starboy756 <taseentariq5@gmail.com>
2025-11-15 10:18:10 +0500

    added new formspree.io key

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 import validator from "validator";
 
 export function Form() {
-  const [state, handleSubmit] = useForm("xknkpqry");
+  const [state, handleSubmit] = useForm("xldabbae");
   const [validEmail, setValidEmail] = useState(false);
   const [isHuman, setIsHuman] = useState(false);
   const [message, setMessage] = useState("");
